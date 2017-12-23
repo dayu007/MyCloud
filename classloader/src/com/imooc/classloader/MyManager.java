d75f9d58f348b9b8cec450719b7c06dfb428994b
dayu007 <1198739880@qq.com>
2017-12-23 20:21:54 +0800

    spring-boot热部署学习

--- classloader/src/com/imooc/classloader/MyManager.java
+++ classloader/src/com/imooc/classloader/MyManager.java
@@ -1,18 +1,18 @@
 package com.imooc.classloader;
+
 /**
  * 实现java类的热加载功能
- * @author dayu 
- * @version 创建时间：2017年12月22日 下午4:44:17
- * 类说明
+ * 
+ * @author dayu
+ * @version 创建时间：2017年12月22日 下午4:44:17 类说明
  */
 public class MyManager implements BaseManager
 {
+	@Override
+	public void logic()
+	{
+		System.out.println("测试打印语句..测试打印语句..");
+		System.out.println("社会主义好！123123");
+	}
 
-    @Override
-    public void logic()
-    {
-        System.out.println("测试打印语句123");
-        System.out.println("打印效果真的好");
-    }
-    
 }
